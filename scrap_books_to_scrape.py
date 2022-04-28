fd8bc06ba992a97e8a3aeec0e5344fa1a503fc63
Nobime <romeo.nobime@mailo.com>
2022-04-28 15:02:43 +0200

    retrieve all product data from a category

--- scrap_books_to_scrape.py
+++ scrap_books_to_scrape.py
@@ -12,19 +12,29 @@ category = []
 review_rating = []
 image_url = []
 
-url = "http://books.toscrape.com/catalogue/its-only-the-himalayas_981/index.html"
-response = requests.get(url)
+catalogue_url = "https://books.toscrape.com/catalogue/"
+travel_url = "https://books.toscrape.com/catalogue/category/books/travel_2/index.html"
+response = requests.get(travel_url)
 
 if response.ok:
     page = response.content
     soup = BeautifulSoup(page, "html.parser")
+    product_page_url = [catalogue_url + h3.find_next('a')['href'].replace('../', '') for h3 in soup.findAll('h3')]
 
-    product_page_url = url
-    product_information = [td.text for td in soup.find('table', class_='table table-striped').findAll('td')]
-    product_title = soup.find('h1').text
-    product_description = soup.find('div', id='product_description').find_next('p').text
-    category = soup.find('ul', class_='breadcrumb').findAll('a')[2].text
-    review_rating = soup.find('p', class_='star-rating')['class'][1]
-    image_url = soup.find('img')['src']
+    for url in product_page_url:
 
-    print(product_information)
+        response = requests.get(url)
+
+        if response.ok:
+            page = response.content
+            soup = BeautifulSoup(page, "html.parser")
+
+            product_page_url = url
+            product_information = [td.text for td in soup.find('table', class_='table table-striped').findAll('td')]
+            product_title = soup.find('h1').text
+            product_description = soup.find('div', id='product_description').find_next('p').text
+            category = soup.find('ul', class_='breadcrumb').findAll('a')[2].text
+            review_rating = soup.find('p', class_='star-rating')['class'][1]
+            image_url = soup.find('img')['src']
+
+            print(product_page_url)
