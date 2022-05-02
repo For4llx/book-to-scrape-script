ea16d6c33affd1512d29bfcc6c4c29e608e43f00
Nobime <romeo.nobime@mailo.com>
2022-05-02 12:19:57 +0200

    create csv file data for every category from book to scrape

--- scrap_books_to_scrape.py
+++ scrap_books_to_scrape.py
@@ -1,19 +1,23 @@
 import requests
 from bs4 import BeautifulSoup
+import csv
 
-product_page_url = []
-universal_product_code = []
-title = []
-price_including_tax = []
-price_excluding_tax = []
-number_available = []
-product_description = []
-category = []
-review_rating = []
-image_url = []
+headers = [
+  'product_page_url',
+  'universal_ product_code',
+  'product_title',
+  'price_including_tax',
+  'price_excluding_tax',
+  'number_available',
+  'product_description',
+  'category',
+  'review_rating',
+  'image_url'
+]
 
 home_url = 'https://books.toscrape.com/index.html'
 response = requests.get(home_url)
+
 if response.ok:
     home_page = response.content
     soup = BeautifulSoup(home_page, "html.parser")
@@ -22,6 +26,17 @@ if response.ok:
     for category_url in categories_url:
         page = 0
         pages = 1
+        product_page_url = []
+        universal_product_code = []
+        product_title = []
+        price_including_tax = []
+        price_excluding_tax = []
+        number_available = []
+        product_description = []
+        category = []
+        review_rating = []
+        image_url = []
+
         while int(page) < int(pages):
             response = requests.get(category_url)
 
@@ -37,20 +52,41 @@ if response.ok:
                     if response.ok:
                         product_page = response.content
                         soup = BeautifulSoup(product_page, "html.parser")
-                        product_page_url = product_url
                         product_information = [td.text for td in soup.find('table', class_='table table-striped').findAll('td')]
-                        product_title = soup.find('h1').text
-                        product_description = soup.find('div', id='product_description').find_next('p').text
-                        category = soup.find('ul', class_='breadcrumb').findAll('a')[2].text
-                        review_rating = soup.find('p', class_='star-rating')['class'][1]
-                        image_url = soup.find('img')['src']
-                        print(product_title)
+                        product_page_url.append(product_url)
+                        universal_product_code.append(product_information[0])
+                        price_including_tax.append(product_information[2])
+                        price_excluding_tax.append(product_information[3])
+                        number_available.append(product_information[5])
+                        product_title.append(soup.find('h1').text)
+                        product_description.append(soup.find('div', id='product_description').find_next('p').text)
+                        category.append(soup.find('ul', class_='breadcrumb').findAll('a')[2].text)
+                        review_rating.append(soup.find('p', class_='star-rating')['class'][1])
+                        image_url.append(soup.find('img')['src'])
 
-            page = page+1
-            if pagination is not None:
-                next_ = pagination.find('li', class_='next')
-                pages = pagination.find('li', class_='current').text.split()[-1]
-                if next_ is not None:
-                    next_ = next_.a['href']
-                    category_url = category_url.replace('index.html', next_)
+                page = page+1
+                if pagination is not None:
+                    next_ = pagination.find('li', class_='next')
+                    pages = pagination.find('li', class_='current').text.split()[-1]
 
+                    if next_ is not None:
+                        next_ = next_.a['href']
+                        category_url = category_url.replace('index.html', next_)
+
+        with open( category[page] + '.csv', 'w') as csv_file:
+            writer = csv.writer(csv_file, delimiter=',')
+            writer.writerow(headers)
+
+            for url, upc, title, price_tax_inc, price_tax_ex, number, description, category, rating, img_url in zip(
+                product_page_url,
+                universal_product_code,
+                product_title,
+                price_including_tax,
+                price_excluding_tax,
+                number_available,
+                product_description,
+                category,
+                review_rating,
+                image_url):
+
+                writer.writerow([url, upc, title, price_tax_inc, price_tax_ex, number, description, category, rating, img_url])
