3395ca5f6217b99493e412eb97d9910cffce6735
Nobime <romeo.nobime@mailo.com>
2022-05-09 16:26:41 +0200

    correction et de bugs

--- scrap_books_to_scrape.py
+++ scrap_books_to_scrape.py
@@ -1,7 +1,26 @@
-import requests, os
+import requests, os, csv
 from bs4 import BeautifulSoup
-import csv
 
+def get_soup(url):
+    response = requests.get(url)
+
+    if response.ok:
+        soup = BeautifulSoup(response.content, 'html.parser')
+        return soup
+    else:
+        print('Error: response is not 200')
+
+def get_next_page(soup):
+    next_page = soup.find('li', class_='next')
+
+    if next_page:
+        next_page = str(next_page.a['href'])
+        return next_page
+    else:
+        next_page = False
+        return next_page
+
+home_url = 'https://books.toscrape.com/index.html'
 headers = [
   'product_page_url',
   'universal_ product_code',
@@ -15,83 +34,70 @@ headers = [
   'image_url'
 ]
 
-home_url = 'https://books.toscrape.com/index.html'
-response = requests.get(home_url)
+soup = get_soup(home_url)
+categories_url = ['https://books.toscrape.com/' + a['href'] for a in soup.find('div', class_='side_categories').findAll('ul')[1].findAll('a')]
 
-if response.ok:
-    home_page = response.content
-    soup = BeautifulSoup(home_page, "html.parser")
-    categories_url = ['https://books.toscrape.com/' + a['href'] for a in soup.find('div', class_='side_categories').findAll('ul')[1].findAll('a')]
+for category_url in categories_url:
+    product_page_url = []
+    universal_product_code = []
+    product_title = []
+    price_including_tax = []
+    price_excluding_tax = []
+    number_available = []
+    product_description = []
+    category = []
+    review_rating = []
+    image_url = []
+    current_page = 'index.html'
 
-    for category_url in categories_url:
-        page = 1
-        pages = 2
-        product_page_url = []
-        universal_product_code = []
-        product_title = []
-        price_including_tax = []
-        price_excluding_tax = []
-        number_available = []
-        product_description = []
-        category = []
-        review_rating = []
-        image_url = []
+    while True:
+        soup_category = get_soup(category_url)
+        products_url = ['https://books.toscrape.com/catalogue/' + h3.find_next('a')['href'].replace('../', '') for h3 in soup_category.findAll('h3')]
 
-        while int(page) < int(pages):
-            response = requests.get(category_url)
+        for product_url in products_url:
+            soup = get_soup(product_url)
+            product_information = [td.text for td in soup.find('table', class_='table table-striped').findAll('td')]
+            product_description_info = soup.find('div', id='product_description')
+            product_page_url.append(product_url)
+            universal_product_code.append(product_information[0])
+            price_including_tax.append(product_information[2])
+            price_excluding_tax.append(product_information[3])
+            number_available.append(product_information[5])
+            product_title.append(soup.find('h1').text)
+            if product_description_info:
+                product_description.append(product_description_info.find_next('p').text)
+            category.append(soup.find('ul', class_='breadcrumb').findAll('a')[2].text)
+            category_name = soup.find('ul', class_='breadcrumb').findAll('a')[2].text
+            review_rating.append(soup.find('p', class_='star-rating')['class'][1])
+            image_url.append('https://books.toscrape.com/' + soup.find('img')['src'].replace('../',''))
+            print(soup.find('h1').text)
 
-            if response.ok:
-                category_page = response.content
-                soup = BeautifulSoup(category_page, "html.parser")
-                products_url = ['https://books.toscrape.com/catalogue/' + h3.find_next('a')['href'].replace('../', '') for h3 in soup.findAll('h3')]
-                pagination = soup.find('ul', class_='pager')
+        next_page = get_next_page(soup_category)
+        print(next_page)
 
-                for product_url in products_url:
-                    response = requests.get(product_url)
+        if next_page:
+            category_url = category_url.replace(current_page, next_page)
+            current_page = next_page
+        else:
+            break
 
-                    if response.ok:
-                        product_page = response.content
-                        soup = BeautifulSoup(product_page, "html.parser")
-                        product_information = [td.text for td in soup.find('table', class_='table table-striped').findAll('td')]
-                        product_description_info = soup.find('div', id='product_description')
-                        product_page_url.append(product_url)
-                        universal_product_code.append(product_information[0])
-                        price_including_tax.append(product_information[2])
-                        price_excluding_tax.append(product_information[3])
-                        number_available.append(product_information[5])
-                        product_title.append(soup.find('h1').text)
-                        if product_description_info is not None:
-                            product_description.append(product_description_info.find_next('p').text)
-                        category.append(soup.find('ul', class_='breadcrumb').findAll('a')[2].text)
-                        review_rating.append(soup.find('p', class_='star-rating')['class'][1])
-                        image_url.append('https://books.toscrape.com/' + soup.find('img')['src'].replace('../',''))
-                        print(soup.find('h1').text)
+    data = 'data/' + category_name + '.csv'
+    os.makedirs(os.path.dirname(data), exist_ok=True)
 
-                page = page+1
-                if pagination is not None:
-                    next_ = pagination.find('li', class_='next')
-                    pages = pagination.find('li', class_='current').text.split()[-1]
+    with open(data, 'w') as csv_file:
+        writer = csv.writer(csv_file, delimiter=',')
+        writer.writerow(headers)
 
-                    if next_ is not None:
-                        next_ = next_.a['href']
-                        category_url = category_url.replace('index.html', next_)
+        for url, upc, title, price_tax_inc, price_tax_ex, number, description, category, rating, img_url in zip(
+            product_page_url,
+            universal_product_code,
+            product_title,
+            price_including_tax,
+            price_excluding_tax,
+            number_available,
+            product_description,
+            category,
+            review_rating,
+            image_url):
 
-        data = 'data/' + category[page] + '.csv'
-        os.makedirs(os.path.dirname(data), exist_ok=True)
-        with open(data, 'w') as csv_file:
-            writer = csv.writer(csv_file, delimiter=',')
-            writer.writerow(headers)
-
-            for url, upc, title, price_tax_inc, price_tax_ex, number, description, category, rating, img_url in zip(
-                product_page_url,
-                universal_product_code,
-                product_title,
-                price_including_tax,
-                price_excluding_tax,
-                number_available,
-                product_description,
-                category,
-                review_rating,
-                image_url):
-
-                writer.writerow([url, upc, title, price_tax_inc, price_tax_ex, number, description, category, rating, img_url])
+            writer.writerow([url, upc, title, price_tax_inc, price_tax_ex, number, description, category, rating, img_url])
